fa37def7a8c0de8f1d143699f015326b77ac2fb3
KlimVladMaks <vkm.blog@yandex.ru>
2023-11-03 01:46:22 +0300

    Добавлены кнопки калькулятора. Добавлен обработчик событий нажатия на кнопку

--- calculator-wpf/MainWindow.xaml.cs
+++ calculator-wpf/MainWindow.xaml.cs
@@ -13,16 +13,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace calculator_wpf
+namespace CalculatorWpf
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Класс для реализации логики взаимодействия для окном MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Метод для работы с окном.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            foreach (UIElement element in MainRoot.Children)
+            {
+                if (element is Button button)
+                {
+                    button.Click += ButtonClick;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод для обработки события нажатия на кнопку калькулятора.
+        /// </summary>
+        /// <param name="sender">Объект, который вызвал данное событие.</param>
+        /// <param name="e">Информация о данном событии.</param>
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var str = (string) ((Button) e.OriginalSource).Content;
+            textLabel.Text = str;
         }
     }
 }
